07330956c7515b7d86b9ce093a20af6ebcd45e09
raphaelcaxias <pires.xx@gmail.com>
2026-06-23 17:52:31 -0300

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from .date_helpers import get_greeting, detect_meal_period, detect_meal_type
-from .validators import validate_registration, validate_weight, validate_height, validate_age
-from .motivational_quotes import get_quote
-
-__all__ = [
-    "get_greeting", "detect_meal_period", "detect_meal_type",
-    "validate_registration", "validate_weight", "validate_height", "validate_age",
-    "get_quote",
-]
