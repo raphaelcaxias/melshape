ef225d2f4535e68aeb380df7c02c1eaa79473055
raphaelcaxias <pires.xx@gmail.com>
2026-07-02 17:22:18 -0300

    Delete core directory

--- core/__init__.py
+++
@@ -1,4 +0,0 @@
-from .database import Database
-from .models import User, Professional, Meal, WeightLog, Supplement, WorkoutLog
-
-__all__ = ["Database", "User", "Professional", "Meal", "WeightLog", "Supplement", "WorkoutLog"]
